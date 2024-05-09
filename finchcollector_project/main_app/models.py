1468a27e28c06b7c4da477f84fbeaa1afc6b203d
cgch8 <carloschie15@gmail.com>
2024-05-09 14:43:50 -0700

    part 2 done

--- finchcollector_project/main_app/models.py
+++ finchcollector_project/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    colors = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+       
